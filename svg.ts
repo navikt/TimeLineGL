afcfedab5cd6a4dfaec53ad57d8f5809e307c775
poke53280 <anders.topper@gmail.com>
2019-12-02 15:27:46 +0100

    More on timeline and reg visualization.

--- svg.ts
+++ svg.ts
@@ -11,22 +11,52 @@ function create_circle(x, y, r) {
     return cir
 }
 
+function create_rectangle(reg: number, fom : number, tom: number, color: string) {
+    const L_y : number = 1000
+    const r = document. createElementNS(svg_url, "rect");
+
+    const nThichkness : number = 25;
+
+    fom = L_y - fom;
+    tom = L_y - tom;
+
+    const length : number = fom - tom;
+
+    fom = fom - length;
+
+    const x_min : number = Math.round(reg - 1.0 * nThichkness/2.0);
+
+    r.setAttribute("x", String(x_min));
+    r.setAttribute("y", String(fom));
+    r.setAttribute("width", String(nThichkness));
+
+    r.setAttribute("height", String(length));
+    r.setAttribute("fill", color);
+    return r;
+}
+
+
 // create the svg element
-const svg1 = document. createElementNS(svg_url, "svg");
+//const svg1 = document. createElementNS(svg_url, "svg");
 
 // set width and height
-svg1.setAttribute("width", "100");
-svg1.setAttribute("height", "100");
+//svg1.setAttribute("width", "1000");
+//svg1.setAttribute("height", "1000");
+var svg1 : HTMLElement = document. getElementById("a");
 
-const cir1 = create_circle("80", "80", "30")
-svg1.appendChild(cir1);
+svg1.appendChild(create_circle("80", "80", "30"));
 
-const cir2 = create_circle("30", "40", "20")
-svg1.appendChild(cir2);
+svg1.appendChild(create_circle("130", "140", "20"));
+
+svg1.appendChild(create_rectangle(240, 240, 330, "red"));
+svg1.appendChild(create_rectangle(340, 340, 360, "red"));
+svg1.appendChild(create_rectangle(330, 540, 560, "blue"));
+svg1.appendChild(create_rectangle(430, 840, 870, "green"));
+
 
 
 // attach container to document
-document. getElementById("svg54583").appendChild(svg1);
+// document. getElementById("aaa").appendChild(svg1);
 
 
 
